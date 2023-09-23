3be45699a4ad7e5a8d49982e8ea314578d1bb17f
marksantander <msntndr.ms@gmail.com>
2023-09-22 22:02:27 -0400

    modified index.js to use npm svg to png

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ SVG Logo Maker
 
 This Node.js command-line application takes in user input to generate a logo and save it as an SVG file.
 
-Dependancy: NPM SVG.JS
+Dependancy: NPM SVG.JS, NPM inquirer, NPM Jest
 
 The application will prompt you for the following information:
 
@@ -12,3 +12,7 @@ The application will prompt you for the following information:
 - Text for the logo
 
 Once user inputs reuired data then the app will generate an SVG file.
+
+Credits: bard.google.com, npmjs.com
+
+Video: https://drive.google.com/file/d/1kfU1Tj5nZmcRyU9epMQM6La4qMfMPuV1/view
--- index.js
+++ index.js
@@ -1,51 +1,65 @@
-const inquirer = require('inquirer');
-const svgjs = require('../Module-10/node_modules/svg.js');
+type: 'module'
+import inquirer from 'inquirer';
+const svgToFile = require('svg-to-png-file');
 
-const generateLogo = async (answers) => {
-  const { color, shape, text } = answers;
+const shapes = ['circle', 'triangle', 'square'];
 
-  const doc = svgjs.Document();
-  const shapeElement = doc.element(shape);
-  shapeElement.attr('fill', color);
+const generateLogo = async (input) => {
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+    <${input.shape} fill="${input.shapeColor}" />
+    <text x="150" y="100" font-size="48" fill="${input.textColor}">${input.text}</text>
+  </svg>`;
 
-  const textElement = doc.text(text);
-  doc.add(shapeElement);
-  doc.add(textElement);
-
-  await doc.save(`logo.${answers.format}`);
-};
-
-const main = async () => {
-  const answers = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'color',
-      message: 'Enter a color for the logo:',
-      //validate: validateColorKeyword,
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Select a shape for the logo:',
-      choices: ['circle', 'rtriangle', 'square'],
-    },
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter text for the logo:',
-      validate: (input) => {
-        if (input.length <= 3) {
-          return true;
-        } else {
-          return 'Text must be no more than 3 characters long.';
-        }
-      },
-    },
-  ]);
-    
-  await generateLogo(answers);
+  await svgToFile.save(svg, 'logo.svg');
 
   console.log('Generated logo.svg');
 };
 
-main();
+(async () => {
+  const input = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter text for your logo (up to 3 characters): ',
+      validate: (value) => {
+        if (value.length > 3) {
+          return 'Text must be up to 3 characters long.';
+        }
+
+        return true;
+      },
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter the text color (keyword or hex): ',
+      validate: (value) => {
+        if (!/^(#[0-9a-fA-F]{6}|[a-z]+)$/.test(value)) {
+          return 'Invalid color.';
+        }
+
+        return true;
+      },
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape for your logo: ',
+      choices: shapes,
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter the shape color (keyword or hex): ',
+      validate: (value) => {
+        if (!/^(#[0-9a-fA-F]{6}|[a-z]+)$/.test(value)) {
+          return 'Invalid color.';
+        }
+
+        return true;
+      },
+    },
+  ]);
+
+  await generateLogo(input);
+})();
